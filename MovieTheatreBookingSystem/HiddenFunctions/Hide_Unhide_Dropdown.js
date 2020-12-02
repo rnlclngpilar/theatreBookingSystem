e854bac8f156acad02d1c224cabbb97fabab6ca4
Indigo4Opal <54689757+Indigo4Opal@users.noreply.github.com>
2020-12-02 16:55:09 -0500

    Update9_Dec2_(4-55pm)_Group_LocateMovies_RelatedFiles

--- MovieTheatreBookingSystem/HiddenFunctions/Hide_Unhide_Dropdown.js
+++ MovieTheatreBookingSystem/HiddenFunctions/Hide_Unhide_Dropdown.js
@@ -6,3 +6,21 @@ function displayDropdown()
     var box1 = document.getElementById("div2");
     if (box1.style.display == "none") { box1.style.display = "block"; }
 }
+
+/*
+
+//code below hides/unhide the movie display
+document.getElementById("btn1").addEventListener("click",function()
+        {
+            var box1 = document.getElementById("div2");
+            if (box1.style.display == "none")
+            {
+                box1.style.display = "block";
+            }
+            else
+            {
+                box1.style.display = "none";
+            }
+
+        })
+*/
