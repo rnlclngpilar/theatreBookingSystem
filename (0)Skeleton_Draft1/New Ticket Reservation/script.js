dca2f67b012273ca98b4f97c11edf3cb0b8c2a3b
rcagui <renzo88@outlook.com>
2020-12-03 02:31:32 -0500

    changes-12-03-2020-2:31am

---
+++ (0)Skeleton_Draft1/New Ticket Reservation/script.js
@@ -0,0 +1,81 @@
+function onLoaderFunc()
+{
+  $(".seatStructure *").prop("disabled", true);
+  $(".displayerBoxes *").prop("disabled", true);
+}
+function takeData()
+{
+  if (( $("#Username").val().length == 0 ) || ( $("#Numseats").val().length == 0 ))
+  {
+  alert("Please Enter your Name and Number of Seats");
+  }
+  else
+  {
+    $(".inputForm *").prop("disabled", true);
+    $(".seatStructure *").prop("disabled", false);
+    document.getElementById("notification").innerHTML = "<b style='margin-bottom:0px;background:yellow;'>Please Select your Seats NOW!</b>";
+  }
+}
+
+
+function updateTextArea() { 
+    
+  if ($("input:checked").length == ($("#Numseats").val()))
+    {
+      $(".seatStructure *").prop("disabled", true);
+      
+     var allNameVals = [];
+     var allNumberVals = [];
+     var allSeatsVals = [];
+  
+     //Storing in Array
+     allNameVals.push($("#Username").val());
+     allNumberVals.push($("#Numseats").val());
+     $('#seatsBlock :checked').each(function() {
+       allSeatsVals.push($(this).val());
+     });
+    
+     //Displaying 
+     $('#nameDisplay').val(allNameVals);
+     $('#NumberDisplay').val(allNumberVals);
+     $('#seatsDisplay').val(allSeatsVals);
+    }
+  else
+    {
+      alert("Please select " + ($("#Numseats").val()) + " seats")
+    }
+  }
+
+
+function myFunction() {
+  alert($("input:checked").length);
+}
+
+/*
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+*/
+
+
+$(":checkbox").click(function() {
+  if ($("input:checked").length == ($("#Numseats").val())) {
+    $(":checkbox").prop('disabled', true);
+    $(':checked').prop('disabled', false);
+  }
+  else
+    {
+      $(":checkbox").prop('disabled', false);
+    }
+});
