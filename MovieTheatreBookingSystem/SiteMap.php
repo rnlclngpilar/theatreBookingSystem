f83d8f7def5d4c19721502a25949403aa8dd8423
Indigo4Opal <54689757+Indigo4Opal@users.noreply.github.com>
2020-12-04 17:46:52 -0500

    CleanFile_last

--- MovieTheatreBookingSystem/SiteMap.php
+++ MovieTheatreBookingSystem/SiteMap.php
@@ -14,9 +14,8 @@
             <legend>Site Map:</legend>
             <li><a href="About/about.html">About</a></li>
             <li><a href="HomePage/homePage.html">Home</a></li>
-            <li><a href="UserPage_Files/enterUserPage.php">Log-in/Sign-up</a></li>              <!-- Copy (..\(0)Skeleton_Draft1\loginForm.php) -->
-            <li><a href="UserPage_Files/userAccountPage.php">Account</a><-----must be logged-in to view</li>             
-            <!-- <li><a href="TicketReservation_Files/ticketReservationForm.html">Quick Booking</a></li>                                               Copy (..\(0)Skeleton_Draft1\receipt\form.html) -->
+            <li><a href="UserPage_Files/enterUserPage.php">Log-in/Sign-up</a></li>
+            <li><a href="UserPage_Files/userAccountPage.php">Account</a><-----must be logged-in to view</li>
             <li><a href="SearchesWithFilters/LocateMovies/LocateMovie.php">Movies</a></li>
             <li>Contact</li>
         </ul>
