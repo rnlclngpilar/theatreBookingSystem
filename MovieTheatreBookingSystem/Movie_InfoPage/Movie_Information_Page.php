9d08c353dea1b2292c737b932e0c765cd65a9978
Indigo4Opal <54689757+Indigo4Opal@users.noreply.github.com>
2020-12-04 10:12:46 -0500

    Update_Dec4_(10-12)_MovieInfo

---
+++ MovieTheatreBookingSystem/Movie_InfoPage/Movie_Information_Page.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"></meta>
+    <link type="text/css" rel="stylesheet" href="Resources/css_Theme/mainTheme.css">
+    <Title>Movie Information</Title> 
+</head>
+<body>
+
+   <table class="mainPageTable">
+	<!-- ~ Movie Information ~ Header -->
+        <tr>
+			<th><h1> ~ Movie Information ~ </h1></th>
+		</tr>
+
+    <!-- Output Movie Poster & Information Block -->
+		<tr>
+			<td class="movieBlock">
+				<?php include './getMovieInfo.php'; ?>
+                    <!-- add image-database? -->
+                    <!-- possibly link theater contact info here-->
+                    <script>
+                        function onClick($movieID) { /* Send them to ticket form and pass to it MovieID */};
+                    </script>
+			</td>
+		</tr>
+</body>
+</html>
---
+++ MovieTheatreBookingSystem/Movie_InfoPage/getMovieInfo.php
@@ -0,0 +1,45 @@
+<?php
+    include './Resources/connection.php';
+
+    //retreive movie details
+    $mysql_movInf = "SELECT * FROM movieInformation";
+    $result = $connection->query($mysql_movInf);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+        $movieID = $row['movieID'];
+
+        $title = $row['title'];
+        $availability = ($row['availability'] == 1) ? $availability="Currently Showing" : $availability="Coming Soon...";
+        $releaseDate = $row['releaseDate'];
+        $rating = $row['rating'];
+        $genre = $row['genre'];
+        $description = $row['description'];
+
+        $threeD = ($row['3D'] == 1) ? $threeD="Yes" : $threeD="No";
+        $cc = ($row['cc']) ? $cc="Yes" : $cc="No";
+        $hour = date('h', strtotime($row['duration']));
+        $min = date('m', strtotime($row['duration']));
+        $duration = "$hour h $min m";
+
+        echo("<table class='poster' onClick($movieID)=''>");
+        echo("<td>");
+        echo("<img src='Resources/movieImgs/movies/".$movieID.".jpg' class='boxImg' height='210px'>"); /* Added height */
+        echo("</td>");
+        echo("<td>");
+        echo("<p><strong>Title:</strong> ".$title."</p>");
+        echo("<p><strong>Availablity:</strong> ".$availability."</p>");
+        echo("<p><strong>Release Date:</strong> ".$releaseDate."</p>");
+        echo("<p><strong>Rating:</strong> ".$rating."</p>");
+        echo("<p><strong>Genre:</strong> ".$genre."</p>");
+        echo("<p><strong>Description:</strong> ".$description."</p>");
+        //echo("<p><strong>3D:</strong> ".$threeD."</p>");
+        echo("<p><strong>Closed Captioning:</strong> ".$cc."</p>");
+        echo("<p><strong>Duration:</strong> ".$duration."</p>");
+        echo("</td>");
+        echo("</table>");
+    } 
+    
+    $connection->close();
+?>
